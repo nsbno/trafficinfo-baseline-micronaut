2b963a7e1782d5f34f0cf82109c44c84f9a97795
stekern <stekern@users.noreply.github.com>
2020-04-30 12:23:58 +0200

    Uncomment terraform code

--- terraform/template/policies.tf
+++ terraform/template/policies.tf
@@ -1,26 +1,26 @@
-#data "aws_iam_policy_document" "cloudwatch_for_microservice" {
-#  statement {
-#    effect = "Allow"
-#    resources = [
-#      "*"
-#    ]
-#    actions = [
-#      "cloudwatch:PutMetricData"
-#    ]
-#  }
-#}
-#
-#data "aws_iam_policy_document" "ssm_for_microservice" {
-#  statement {
-#    effect = "Allow"
-#    actions = [
-#      "ssm:GetParameters",
-#      "ssm:GetParameter",
-#      "ssm:GetParameterHistory",
-#      "ssm:GetParametersByPath"
-#    ]
-#    resources = [
-#      "arn:aws:ssm:eu-west-1:${local.current_account_id}:parameter/${var.name_prefix}/config/*",
-#    ]
-#  }
-#}
+data "aws_iam_policy_document" "cloudwatch_for_microservice" {
+  statement {
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+    actions = [
+      "cloudwatch:PutMetricData"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "ssm_for_microservice" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameters",
+      "ssm:GetParameter",
+      "ssm:GetParameterHistory",
+      "ssm:GetParametersByPath"
+    ]
+    resources = [
+      "arn:aws:ssm:eu-west-1:${local.current_account_id}:parameter/${var.name_prefix}/config/*",
+    ]
+  }
+}
