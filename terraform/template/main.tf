80de831d9ac5481b2b0488550fedcd65c5a61425
Erlend Ekern <stekern@users.noreply.github.com>
2020-05-06 10:23:29 +0200

    Use external ecs-microservice module (#81)

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -5,23 +5,8 @@ data "aws_region" "current" {}
 locals {
   current_account_id = data.aws_caller_identity.current-account.account_id
   current_region     = data.aws_region.current.name
+  service_account_id = "929368261477"
   shared_config      = jsondecode(data.aws_ssm_parameter.shared_config.value)
-
-}
-
-# TODO: Resources from `trafficinfo-aws/terraform/modules/template/{kernel-kms.tf,svc-baseline.tf}`
-
-resource "aws_ssm_parameter" "testvariable" {
-  name      = "/${var.name_prefix}/config/${var.application_name}/testvariable"
-  type      = "String"
-  value     = "test"
-  overwrite = true
-}
-
-resource "aws_kms_key" "baseline_params_key" {}
-resource "aws_kms_alias" "baseline_params_key_alias" {
-  target_key_id = aws_kms_key.baseline_params_key.key_id
-  name          = "alias/${var.name_prefix}-${var.application_name}_params_key"
 }
 
 ##################################
@@ -33,236 +18,72 @@ data "aws_ssm_parameter" "shared_config" {
   name = "/trafficinfo/shared_application_config"
 }
 
-
-
 ##################################
 #                                #
 # Microservice                   #
 #                                #
 ##################################
-module "baseline-micronaut" {
-  source                          = "github.com/nsbno/terraform-aws-ecs-fargate?ref=06e632c"
-  name_prefix                     = "${var.name_prefix}-${var.application_name}"
-  vpc_id                          = local.shared_config.vpc_id
-  private_subnet_ids              = local.shared_config.private_subnet_ids
-  lb_arn                          = local.shared_config.lb_arn
-  cluster_id                      = local.shared_config.ecs_cluster_id
-  task_container_image            = "929368261477.dkr.ecr.eu-west-1.amazonaws.com/${var.name_prefix}-${var.application_name}:${data.aws_ssm_parameter.version.value}-SHA1"
-  task_container_port             = 8080
-  task_container_assign_public_ip = true
-  health_check = {
-    port = 8080
-    path = "/${var.application_name}/health"
+module "ecs-microservice" {
+  source             = "github.com/nsbno/terraform-aws-trafficinfo?ref=d87d9d8/ecs-microservice"
+  application-config = "" # Not being used by anything
+  ecs_cluster = {
+    id   = local.shared_config.ecs_cluster_id
+    name = local.shared_config.ecs_cluster_name
   }
-  health_check_grace_period_seconds = 30
-  tags                              = var.tags
-}
-
-resource "aws_lb_listener_rule" "this" {
-  listener_arn = local.shared_config.lb_listener_arn
-  priority     = 340
-
-  action {
-    type             = "forward"
-    target_group_arn = module.baseline-micronaut.target_group_arn
+  health_check_path    = "/${var.application_name}/health"
+  health_check_port    = 8080
+  name_prefix          = var.name_prefix
+  current_account      = local.current_account_id
+  service_name         = var.application_name
+  service_port         = 8080
+  task_container_image = "${local.service_account_id}.dkr.ecr.${local.current_region}.amazonaws.com/${var.name_prefix}-${var.application_name}:${data.aws_ssm_parameter.version.value}-SHA1"
+  task_container_port  = 8080
+  vpc = {
+    private_subnet_ids = local.shared_config.private_subnet_ids
+    vpc_id             = local.shared_config.vpc_id
   }
-
-  condition {
-    path_pattern {
-      values = ["/${var.application_name}/*"]
-    }
+  alb = {
+    arn               = local.shared_config.lb_arn
+    arn_suffix        = local.shared_config.lb_arn_suffix
+    security_group_id = local.shared_config.lb_security_group_id
   }
-}
+  alb_http_listener = {
+    arn = local.shared_config.lb_listener_arn
+  }
+  alb_priority = 340
 
-resource "aws_security_group_rule" "incoming" {
-  security_group_id        = module.baseline-micronaut.service_sg_id
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 8080
-  to_port                  = 8080
-  source_security_group_id = local.shared_config.lb_security_group_id
-}
-
-data "aws_ssm_parameter" "version" {
-  name = "/${var.name_prefix}/${var.name_prefix}-${var.application_name}"
-}
-
-
-resource "aws_iam_role_policy" "cloudwatch_to_microservice" {
-  policy = data.aws_iam_policy_document.cloudwatch_for_microservice.json
-  role   = module.baseline-micronaut.task_role_name
-}
-
-resource "aws_iam_role_policy" "ssm_to_microservice" {
-  policy = data.aws_iam_policy_document.ssm_for_microservice.json
-  role   = module.baseline-micronaut.task_role_name
-}
-
-resource "aws_iam_role_policy" "kms_to_microservice" {
-  policy = data.aws_iam_policy_document.kms_for_microservice.json
-  role   = module.baseline-micronaut.task_role_name
-}
-
-
-##################################
-#                                #
-# API Gateway                    #
-#                                #
-##################################
-locals {
-  api_gateway_schema = templatefile("../static/openapi/baseline.yml", {
+  schema = templatefile("../static/openapi/baseline.yml", {
     hosted_zone_name = local.shared_config.hosted_zone_name
     basePath         = var.application_name
     provider_arn     = local.shared_config.user_pool_arn
   })
-}
-resource "aws_api_gateway_rest_api" "api_gateway_microservice_rest_api" {
-  name = "${var.name_prefix}-${var.application_name}"
-  body = local.api_gateway_schema
-}
 
-resource "aws_api_gateway_deployment" "api_gateway_microservice_rest_api_deployment_v1" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_microservice_rest_api.id
-  stage_name  = "v1"
-  variables = {
-    hash = sha256(local.api_gateway_schema)
-  }
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_base_path_mapping" "gateway_base_path_mapping" {
-  api_id      = aws_api_gateway_rest_api.api_gateway_microservice_rest_api.id
-  stage_name  = aws_api_gateway_deployment.api_gateway_microservice_rest_api_deployment_v1.stage_name
-  domain_name = "services.${local.shared_config.hosted_zone_name}"
   base_path   = var.application_name
-}
+  domain_name = "services.${local.shared_config.hosted_zone_name}"
 
+  tags = var.tags
 
-##################################
-#                                #
-# CloudWatch alarms              #
-#                                #
-##################################
-resource "aws_cloudwatch_metric_alarm" "service_unhealthy" {
-  metric_name         = "UnHealthyHostCount"
-  alarm_name          = "${var.name_prefix}-${var.application_name}-unhealthy"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
-  threshold           = 1
-  namespace           = "AWS/ApplicationELB"
-  dimensions = {
-    TargetGroup  = module.baseline-micronaut.target_group_arn_suffix
-    LoadBalancer = local.shared_config.lb_arn_suffix
-  }
-  period            = 60
-  statistic         = "Average"
-  alarm_description = "${var.name_prefix}-${var.application_name} service has unhealthy targets"
-  tags              = var.tags
-  alarm_actions     = [local.shared_config.alarm_sns_topic_arn]
-  ok_actions        = [local.shared_config.alarm_sns_topic_arn]
-}
+  sqs_queues           = []
+  sns_subscribe_topics = []
+  encryption_keys      = [aws_kms_key.baseline_params_key.arn]
+  s3_read_buckets      = []
+  alarms_sns_topic_arn = [local.shared_config.alarm_sns_topic_arn]
+  hosted_zone_name     = local.shared_config.hosted_zone_name
 
-resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
-  metric_name         = "CPUUtilization"
-  alarm_name          = "${var.name_prefix}-${var.application_name}-cpu"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 5
-  threshold           = 80
-  namespace           = "AWS/ECS"
-  dimensions = {
-    ClusterName = local.shared_config.ecs_cluster_name
-    ServiceName = "${var.name_prefix}-${var.application_name}"
-  }
-  period            = 60
-  statistic         = "Average"
-  alarm_description = "${var.name_prefix}-${var.application_name} has crossed the CPU usage treshold"
-  tags              = var.tags
-  alarm_actions     = [local.shared_config.alarm_sns_topic_arn]
-  ok_actions        = [local.shared_config.alarm_sns_topic_arn]
-}
+  ##################
+  # Added Cognito configuration as example of how to configura a service
+  # with authentication and authorization.
+  #
+  # Cognito user pool to create resources in.
+  user_pool_id = local.shared_config.user_pool_id
 
-resource "aws_cloudwatch_metric_alarm" "high_memory_utilization" {
-  metric_name         = "MemoryUtilization"
-  alarm_name          = "${var.name_prefix}-${var.application_name}-memory"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 5
-  threshold           = 80
-  namespace           = "AWS/ECS"
-  dimensions = {
-    ClusterName = local.shared_config.ecs_cluster_name
-    ServiceName = "${var.name_prefix}-${var.application_name}"
-  }
-  period            = 60
-  statistic         = "Average"
-  alarm_description = "${var.name_prefix}-${var.application_name} has crossed the memory usage treshold"
-  tags              = var.tags
-  alarm_actions     = [local.shared_config.alarm_sns_topic_arn]
-  ok_actions        = [local.shared_config.alarm_sns_topic_arn]
-}
+  # Enabled to create a resource server for the microservice in Cognito.
+  create_resource_server = 1
 
-resource "aws_cloudwatch_metric_alarm" "high_latency" {
-  metric_name         = "IntegrationLatency"
-  alarm_name          = "${var.name_prefix}-${var.application_name}-latency"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 5
-  threshold           = 2000
-  namespace           = "AWS/ApiGateway"
-  dimensions = {
-    ApiName = "${var.name_prefix}-${var.application_name}"
-  }
-  period            = 60
-  statistic         = "Average"
-  alarm_description = "${var.name_prefix}-${var.application_name} latency is above configured treshold"
-  tags              = var.tags
-  alarm_actions     = [local.shared_config.alarm_sns_topic_arn]
-  ok_actions        = [local.shared_config.alarm_sns_topic_arn]
-}
+  # Enabled to create an app client for the microservice in Cognito..
+  create_app_client = 1
 
-resource "aws_cloudwatch_metric_alarm" "num_errors_service" {
-  metric_name         = "5XXError"
-  alarm_name          = "${var.name_prefix}-${var.application_name}-errors-service"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
-  threshold           = 50
-  namespace           = "AWS/ApiGateway"
-  dimensions = {
-    ApiName = "${var.name_prefix}-${var.application_name}"
-  }
-  period            = 60
-  statistic         = "Average"
-  alarm_description = "${var.name_prefix}-${var.application_name} has crossed the 5xx error treshold"
-  tags              = var.tags
-  alarm_actions     = [local.shared_config.alarm_sns_topic_arn]
-  ok_actions        = [local.shared_config.alarm_sns_topic_arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "num_error_logs" {
-  metric_name         = "logback.events.count"
-  alarm_name          = "${var.name_prefix}-${var.application_name}-errors-log"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
-  threshold           = 50
-  namespace           = "${var.name_prefix}-${var.application_name}"
-  dimensions = {
-    level = "error"
-  }
-  period            = 60
-  statistic         = "Sum"
-  alarm_description = "${var.name_prefix}-${var.application_name} has logged to many errors"
-  tags              = var.tags
-  alarm_actions     = [local.shared_config.alarm_sns_topic_arn]
-  ok_actions        = [local.shared_config.alarm_sns_topic_arn]
-}
-
-
-##################################
-#                                #
-# Cognito                        #
-#                                #
-##################################
-locals {
+  # resource server scopes, just for testing.
   resource_server_scopes = {
     read_scope = {
       "scope_name" : "read"
@@ -273,58 +94,29 @@ locals {
       "scope_description" : "write scope for the service."
     }
   }
+
+  # To generate a appclient, you need at least one scope for it.
+  # Baseline has access to the Whoami Service, and also itself.
   app_client_scopes = [
     "https://services.${local.shared_config.hosted_zone_name}/whoami/read",
     "https://services.${local.shared_config.hosted_zone_name}/${var.application_name}/read"
   ]
 }
 
-resource "aws_cognito_resource_server" "resource_server" {
-  identifier = "https://services.${local.shared_config.hosted_zone_name}/${var.application_name}"
-  name       = "${var.name_prefix}-${var.application_name}"
-
-  dynamic "scope" {
-    for_each = [for key, value in local.resource_server_scopes : {
-      scope_name        = value.scope_name
-      scope_description = value.scope_description
-    }]
-
-    content {
-      scope_name        = scope.value.scope_name
-      scope_description = scope.value.scope_description
-    }
-  }
-
-  user_pool_id = local.shared_config.user_pool_id
+data "aws_ssm_parameter" "version" {
+  name = "/${var.name_prefix}/${var.name_prefix}-${var.application_name}"
 }
 
-resource "aws_cognito_user_pool_client" "app_client" {
-  depends_on                           = [aws_cognito_resource_server.resource_server]
-  name                                 = "${var.name_prefix}-${var.application_name}-client"
-  user_pool_id                         = local.shared_config.user_pool_id
-  generate_secret                      = true
-  explicit_auth_flows                  = ["ADMIN_NO_SRP_AUTH"]
-  allowed_oauth_flows                  = ["client_credentials"]
-  allowed_oauth_scopes                 = local.app_client_scopes
-  allowed_oauth_flows_user_pool_client = true
-}
-
-resource "aws_ssm_parameter" "cognito-clientid" {
-  name      = "/${var.name_prefix}/config/${var.application_name}/cognito/clientId"
+# TODO: Resources from `trafficinfo-aws/terraform/modules/template/{kernel-kms.tf,svc-baseline.tf}`
+resource "aws_ssm_parameter" "testvariable" {
+  name      = "/${var.name_prefix}/config/${var.application_name}/testvariable"
   type      = "String"
-  value     = aws_cognito_user_pool_client.app_client.id
+  value     = "test"
   overwrite = true
 }
 
-resource "aws_ssm_parameter" "cognito-clientsecret" {
-  name      = "/${var.name_prefix}/config/${var.application_name}/cognito/clientSecret"
-  type      = "String"
-  value     = aws_cognito_user_pool_client.app_client.client_secret
-  overwrite = true
-}
-
-resource "aws_ssm_parameter" "cognito-url" {
-  name  = "/${var.name_prefix}/config/${var.application_name}/cognito/url"
-  type  = "String"
-  value = "https://auth.${local.shared_config.hosted_zone_name}"
+resource "aws_kms_key" "baseline_params_key" {}
+resource "aws_kms_alias" "baseline_params_key_alias" {
+  target_key_id = aws_kms_key.baseline_params_key.key_id
+  name          = "alias/${var.name_prefix}-${var.application_name}_params_key"
 }
