e6d02a9873949c5409ecb8ab8e1a04d6611c85bf
Daniel <daniel@engfeldt.net>
2020-05-14 14:36:50 +0200

    fix imports

--- src/test/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/HealthControllerTest.kt
+++ src/test/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/HealthControllerTest.kt
@@ -5,12 +5,12 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
-import java.net.URI
-import javax.inject.Inject
 import no.vy.trafficinfo.baseline.micronaut.domain.Health
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
+import java.net.URI
+import javax.inject.Inject
 
 @MicronautTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
