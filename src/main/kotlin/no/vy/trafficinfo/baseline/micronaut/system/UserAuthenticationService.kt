7fa1b86579b32e7174bb87c10e7f289edbcbf3ff
Daniel <daniel@engfeldt.net>
2020-05-15 12:49:12 +0200

    Re-enable UserAuthenticationService, seems to work now with all dependencies.

--- src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/system/UserAuthenticationService.kt
+++ src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/system/UserAuthenticationService.kt
@@ -1,25 +1,25 @@
 package no.vy.trafficinfo.baseline.micronaut.system
 
+import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.*
+import io.reactivex.Flowable
+import org.reactivestreams.Publisher
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
 /**
- * This is a user service only used during tests and as an example class.
- * It contains a hard coded test user which you can add roles to.
+ * This is a Mock Authentication Provider only used during tests and as an example class.
+ * It contains a hard coded test user which you can add roles to when doing local development
+ * so that you dont need to retrieve tokens to access your service on localhost.
  *
- * In the cloud env, this class should not be used at all because
- * instead OAuth and tokens are used for user authentication.
- *
- * TODO fix for 2.0 micronaut.
+ * In the cloud env, this class should not be used at all because instead OAuth tokens
+ * from the Cognito service are used for user authentication.
  */
 @Singleton
-class UserAuthenticationService {
+class UserAuthenticationService : AuthenticationProvider {
     private val log: Logger = LoggerFactory.getLogger(UserAuthenticationService::class.java)
-
-/*
-   override fun authenticate(authenticationRequest: AuthenticationRequest<*, *>?): Publisher<AuthenticationResponse> {
+    override fun authenticate(httpRequest: HttpRequest<*>?, authenticationRequest: AuthenticationRequest<*, *>?): Publisher<AuthenticationResponse> {
         log.info("authentication request from user: ${authenticationRequest?.identity}")
 
         if (authenticationRequest?.identity == "user" && authenticationRequest.secret == "password") {
@@ -27,5 +27,4 @@ class UserAuthenticationService {
         }
         return Flowable.just(AuthenticationFailed())
     }
-*/
 }
