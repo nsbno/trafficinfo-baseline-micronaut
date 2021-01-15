580e1824c09b110332f17b9fd629ecd2bb0e752b
geirbye <geir.byevy.no>
2021-01-15 15:42:17 +0100

    update provider version to 3.21.0

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 provider "aws" {
-  version             = "2.58.0"
+  version             = "3.21.0"
   region              = "eu-west-1"
   allowed_account_ids = ["535719329059"]
 }
