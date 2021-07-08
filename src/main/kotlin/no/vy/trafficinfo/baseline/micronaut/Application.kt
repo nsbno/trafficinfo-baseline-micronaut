13a1f616103d4f43aa74f40c8fc853f911723bbd
Daniel <daniel@engfeldt.net>
2021-07-08 16:07:13 +0200

    Add aws credentials initializer

--- src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/Application.kt
+++ src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/Application.kt
@@ -4,6 +4,8 @@ import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Main application object
@@ -18,12 +20,32 @@ import io.swagger.v3.oas.annotations.info.Info
     )
 )
 object Application {
+    private val log: Logger = LoggerFactory.getLogger(Application::class.java)
 
     @JvmStatic
     fun main(args: Array<String>) {
+// FIXME: 08.07.2021 this should be configurable by args or system properties
+// to be toggleable externally on / off.
+//      initAwsCredentials()
+
         Micronaut.build()
             .packages("no.vy.trafficinfo.baseline.micronaut")
             .mainClass(Application.javaClass)
             .start()
     }
+
+    /**
+     * Set AWS Credentials Profile to be used when doing local development.
+     * This will enable the service to use your local credentials to gain
+     * access to the AWS account that you have logged into.
+     *
+     * It will still fail if the service accesses databases and such that
+     * reside in the internal VPC in the account that has no external access.
+     */
+    private fun initAwsCredentials() {
+        val awsProfile = "dev"
+        System.setProperty("aws.profile", awsProfile)
+        log.info("AWS profile $awsProfile set")
+    }
+
 }
