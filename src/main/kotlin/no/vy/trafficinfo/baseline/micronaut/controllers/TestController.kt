d5409b139945f61527bfe7883026861dbe9a0742
Daniel <daniel@engfeldt.net>
2021-09-21 16:13:51 +0200

    RP-70 fix ktlint

--- src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/TestController.kt
+++ src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/TestController.kt
@@ -1,24 +1,12 @@
 package no.vy.trafficinfo.baseline.micronaut.controllers
 
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.MediaType
-import io.micronaut.http.annotation.*
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
-import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
-import io.reactivex.Single
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.extensions.Extension
-import io.swagger.v3.oas.annotations.extensions.ExtensionProperty
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import no.vy.trafficinfo.baseline.micronaut.domain.Health
-import no.vy.trafficinfo.baseline.micronaut.services.CallbackClient
-import no.vy.trafficinfo.baseline.micronaut.services.WhoamiClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import javax.annotation.security.RolesAllowed
-import javax.inject.Inject
 
 /**
  *
