fd7334575e0a8ccdd2be7ad6d576bb901fcb0a43
Daniel <daniel@engfeldt.net>
2021-09-21 10:03:00 +0200

    RP-70 ktlint reformat

--- src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/CrashController.kt
+++ src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/CrashController.kt
@@ -3,22 +3,8 @@ package no.vy.trafficinfo.baseline.micronaut.controllers
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
-import io.micronaut.security.annotation.Secured
-import io.micronaut.security.authentication.Authentication
-import io.micronaut.security.rules.SecurityRule
-import io.reactivex.Single
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.extensions.Extension
-import io.swagger.v3.oas.annotations.extensions.ExtensionProperty
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import no.vy.trafficinfo.baseline.micronaut.domain.Health
-import no.vy.trafficinfo.baseline.micronaut.services.CallbackClient
-import no.vy.trafficinfo.baseline.micronaut.services.WhoamiClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import javax.annotation.security.RolesAllowed
-import javax.inject.Inject
 
 /**
  * A resource that will throw a RuntimeException and log an ERROR when called.
@@ -35,8 +21,8 @@ class CrashController {
     @Get("/")
     @Produces(MediaType.APPLICATION_JSON)
     fun crash(): HttpResponse<String> {
-       return HttpResponse.serverError("Crash was called.").also {
-           log.error("Crash was called.")
-       }
+        return HttpResponse.serverError("Crash was called.").also {
+            log.error("Crash was called.")
+        }
     }
 }
