e865314a952dca7fac0611669db923653b9fda86
Fredrik Hov <37329363+frehov@users.noreply.github.com>
2021-10-17 17:07:21 +0200

    Upgrade the baseline to Micronaut 3.1 and general cleanup (#291)
    * Upgrade the baseline to Micronaut 3.1 and general cleanup
    
    * Update circleci config
    
    * Update circleci config
    
    * test build with new orb version
    
    * bump version of vydev/trafficinfo-orb to 0.2.0
    
    Co-authored-by: Daniel <daniel@engfeldt.net>

--- src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/services/CallbackClient.kt
+++ src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/services/CallbackClient.kt
@@ -3,13 +3,12 @@ package no.vy.trafficinfo.baseline.micronaut.services
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
-import io.reactivex.Single
-import no.vy.trafficinfo.common.security.client.filters.AccessTokenAuth
+import reactor.core.publisher.Mono
 
-@AccessTokenAuth
+// @AccessTokenAuth TODO Change this to a logging-filter
 @Client("baseline-micronaut")
 interface CallbackClient : CallbackOperations {
 
     @Post("/secured/callback")
-    override fun callback(@Body text: String): Single<String>
+    override fun callback(@Body text: String): Mono<String>
 }
--- src/test/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/HealthControllerTest.kt
+++ src/test/kotlin/no/vy/trafficinfo/baseline/micronaut/controllers/HealthControllerTest.kt
@@ -2,27 +2,41 @@ package no.vy.trafficinfo.baseline.micronaut.controllers
 
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
-import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.security.authentication.Authentication
+import io.micronaut.security.token.jwt.validator.JwtTokenValidator
+import io.micronaut.security.token.validator.TokenValidator
+import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.mockk.every
+import io.mockk.mockk
 import no.vy.trafficinfo.baseline.micronaut.domain.Health
-import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
+import reactor.core.publisher.Flux
+import jakarta.inject.Inject
 import java.net.URI
-import javax.inject.Inject
+import java.util.UUID
 
 @MicronautTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HealthControllerTest {
-    @Inject
-    lateinit var server: EmbeddedServer
-    lateinit var client: HttpClient
 
-    @BeforeAll
-    fun init() {
-        client = HttpClient.create(server.url)
+    @MockBean(JwtTokenValidator::class)
+    fun tokenvalidator(): JwtTokenValidator {
+        return mockk()
     }
+
+    @Inject
+    lateinit var tokenValidator: TokenValidator
+
+    @Inject
+    @field:Client("/")
+    lateinit var client: HttpClient
 
     @Test
     fun testHealthResponse() {
@@ -38,9 +52,23 @@ class HealthControllerTest {
     }
 
     @Test
-    @Disabled
-    fun testSecuredHealthResponseWithAuth() {
+    fun testSecuredHealthResponseWithBasicAuth() {
         val req: HttpRequest<Health> = HttpRequest.GET<Health>(URI.create("/secured/health")).basicAuth("user", "password")
+        val rsp: Health = client.toBlocking().retrieve(req, Health::class.java)
+
+        assertNotNull(rsp.service)
+        assertNotNull(rsp.now)
+        assertNotNull(rsp.runningSince)
+        assertNotNull(rsp.version)
+    }
+
+    @Test
+    fun testSecuredHealthResponseWithBearerAuth() {
+        every {
+            tokenValidator.validateToken(or(ofType(String::class), isNull()), ofType(HttpRequest::class))
+        } returns Flux.just(Authentication.build("user", listOf("https://services.trafficinfo.vydev.io/baseline-micronaut/read"), emptyMap()))
+
+        val req: HttpRequest<Health> = HttpRequest.GET<Health>(URI.create("/secured/health")).bearerAuth(UUID.randomUUID().toString())
         val rsp: Health = client.toBlocking().retrieve(req, Health::class.java)
 
         assertNotNull(rsp.service)
