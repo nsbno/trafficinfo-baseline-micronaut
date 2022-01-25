33b41e98b03a87bcfae0bb7ac31854724984f7cf
Daniel <daniel@engfeldt.net>
2022-01-25 11:18:47 +0100

    remove unused imports.

--- src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/Application.kt
+++ src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/Application.kt
@@ -11,8 +11,6 @@ import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.servers.Server
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 /**
  * Main application object
