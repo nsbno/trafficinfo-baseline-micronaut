148893eaea614702cee582f39b3f34212a1863c4
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-02-01 17:16:22 +0100

    Move service definition to separate module

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -78,26 +78,36 @@ module "api_gateway" {
   enable_xray = true
 }
 
+data "aws_ecr_repository" "this" {
+  registry_id = local.service_account_id
+  name        = "${var.name_prefix}-${var.application_name}"
+}
+
+module "service" {
+  source = "github.com/nsbno/terraform-aws-ecs-service?ref=0.3.1"
+
+  name_prefix = var.application_name
+
+  vpc_id             = local.shared_config.vpc_id
+  private_subnet_ids = local.shared_config.private_subnet_ids
+  cluster_id         = local.shared_config.ecs_cluster_id
+
+  application_container = {
+    name     = "main"
+    image    = "${data.aws_ecr_repository.this.repository_url}:${var.task_container_image}"
+    port     = 8080
+    protocol = "HTTP"
+  }
+
+  lb_health_check = {
+    path = "${var.application_name}/health"
+  }
+}
+
 module "ecs-microservice" {
   source             = "github.com/nsbno/terraform-aws-trafficinfo?ref=d952ae1830215a98513089c8fa19c7307fee3b10/ecs-microservice"
   environment        = var.environment
   application-config = "" # Not being used by anything
-  ecs_cluster = {
-    id   = local.shared_config.ecs_cluster_id
-    name = local.shared_config.ecs_cluster_name
-  }
-  health_check_path    = "/${var.application_name}/health"
-  health_check_port    = 8080
-  name_prefix          = var.name_prefix
-  current_account      = local.current_account_id
-  service_name         = var.application_name
-  service_port         = 8080
-  task_container_image = "${local.service_account_id}.dkr.ecr.${local.current_region}.amazonaws.com/${var.name_prefix}-${var.application_name}:${var.task_container_image}"
-  task_container_port  = 8080
-  vpc = {
-    private_subnet_ids = local.shared_config.private_subnet_ids
-    vpc_id             = local.shared_config.vpc_id
-  }
   alb = {
     arn               = local.shared_config.lb_arn
     arn_suffix        = local.shared_config.lb_arn_suffix
