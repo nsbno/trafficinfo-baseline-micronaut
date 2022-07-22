b3ce88c79215d274b4ba46bac03caf9ede269f70
Daniel <daniel@engfeldt.net>
2022-07-22 11:47:32 +0200

    refactor gradle build into multiple modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,4 @@ pluginManagement {
         id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
     }
 }
+include("baseline:cli", "baseline:domain", "baseline:client", "baseline:micronaut-reactor")
