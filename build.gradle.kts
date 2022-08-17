78dbaa02afc41738216e172eeb6053c3ff809d03
Daniel <daniel@engfeldt.net>
2022-08-17 20:08:39 +0200

    test tracing

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("io.micronaut:micronaut-inject")
     implementation("io.micronaut:micronaut-validation")
     implementation("io.micronaut:micronaut-http-client")
-    implementation("io.micronaut:micronaut-tracing")
+    implementation("io.micronaut.tracing:micronaut-tracing-core")
     implementation("io.micronaut:micronaut-management")
     implementation("io.micronaut.discovery:micronaut-discovery-client")
     implementation("io.micronaut.micrometer:micronaut-micrometer-core")
@@ -113,6 +113,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names")
     implementation("com.fasterxml.jackson.module:jackson-module-blackbird")
     implementation("co.elastic.apm:apm-agent-api:1.30.1")
+    implementation("co.elastic.apm:apm-opentracing:1.33.0")
 
     /**
      * Test dependency configurations.
---
+++ src/main/kotlin/no/vy/trafficinfo/baseline/micronaut/system/ElasticApmTraceFactory.kt
@@ -0,0 +1,26 @@
+package no.vy.trafficinfo.baseline.micronaut.system
+
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Primary
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.context.ServerRequestContext
+import co.elastic.apm.opentracing.ElasticApmTracer
+import io.opentracing.Tracer
+import jakarta.inject.Singleton
+
+@Factory
+class ElasticApmTracerFactory {
+    @Singleton
+    @Primary
+    fun elasticApmTracer(): Tracer {
+        return object : ElasticApmTracer() {
+            override fun buildSpan(operationName: String?): Tracer.SpanBuilder {
+                val spanBuilder: Tracer.SpanBuilder = super.buildSpan(operationName)
+                val httpUrl = ServerRequestContext.currentRequest<Any?>()
+                    .map { request: HttpRequest<Any?> -> request.uri.toString() }
+                    .orElse("http://unknown.host/couldNotFindRequestContext")
+                return spanBuilder.withTag("http.url", httpUrl)
+            }
+        }
+    }
+}
